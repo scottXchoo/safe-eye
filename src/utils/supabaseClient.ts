addb11755cbfedcb965a1f4faebed5c47b5ae944
scottXchoo <ckh0601@cau.ac.kr>
2024-04-02 17:52:09 +0900

    feat(supabaseClient): add supabaseClient in utils file

---
+++ src/utils/supabaseClient.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  throw new Error("Supabase URL and Anon Key must be defined");
+}
+
+const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export { supabase };
