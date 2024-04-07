8af0bcf6e3b79952e9e8cefbd65818eed795db05
scottXchoo <ckh0601@cau.ac.kr>
2024-04-07 17:21:22 +0900

    chore(auth/confirm/route) : request => req

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { EmailOtpType } from "@supabase/supabase-js";
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType;
   const next = searchParams.get("next");
