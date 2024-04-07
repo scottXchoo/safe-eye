939478d3ee9b20e8c34633e49a3d9af2a8af3068
scottXchoo <ckh0601@cau.ac.kr>
2024-04-07 09:48:27 +0900

    fix(build) : Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -85,7 +85,7 @@ export default function AuthForm() {
           </>
         ) : (
           <>
-            Don't have an account?{" "}
+            {"Don't have an account?"}{" "}
             <button
               type="button"
               onClick={() => setIsNewUser(true)}
